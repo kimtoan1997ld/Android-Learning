31cd241b7489a0c042f4bd788a258841fa5d3014
= <=>
2018-09-10 14:57:10 +0700

    Learn EditText

--- EditText/app/src/main/java/com/kimtoan/edittext/MainActivity.java
+++ EditText/app/src/main/java/com/kimtoan/edittext/MainActivity.java
@@ -1,29 +1,29 @@
-package com.kimtoan.button;
+package com.kimtoan.edittext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtText;
-    Button bntClick;
+    EditText edtText;
+    Button bntButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtText = (TextView)findViewById(R.id.textView);
-        bntClick = (Button)findViewById(R.id.button);
-
-        bntClick.setOnClickListener(new View.OnClickListener() {
+        edtText = (EditText) findViewById(R.id.editText);
+        bntButton = (Button) findViewById(R.id.button);
+        bntButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtText.setText("Leaning Android");
+                Toast.makeText(MainActivity.this, edtText.getText().toString(), Toast.LENGTH_LONG).show();
             }
         });
     }
 }
-
